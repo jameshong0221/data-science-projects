0439ca84f0cfc39f2b458c38d455855584c8ee55
James Hong <joonpyo.hong92@gmail.com>
2023-07-18 17:52:26 +0900

    Pull ETH transaction history and balance

--- DeSpread/3ridge/wallet_analytics.py
+++ DeSpread/3ridge/wallet_analytics.py
@@ -1,35 +1,51 @@
 import pandas as pd
 import requests
-from pandas_profiling import ProfileReport
+import json
+#from pandas_profiling import ProfileReport
 
 # Load 3ridge data
 df = pd.read_csv("3ridge-web.users.csv")
 df.info()
-df.head()
+df.head(2)
 
 # Exploratory Analysis - Data profiling report
-profile = ProfileReport(df)
-profile.to_notebook_iframe()
+#profile = ProfileReport(df)
+#profile.to_notebook_iframe()
 
 df_wallet_0 = df["wallets[0].address"]
 df_wallet_1 = df["wallets[1].address"]
 df_wallet_2 = df["wallets[2].address"]
 
+df_rewardPoint = df["rewardPoint"]
+df_rewardPoint.head(20)
+df_rewardPoint.info()
+
 df_wallet_0.dropna(inplace=True)
 df_wallet_0.head()
+df_wallet_0.info()
 df_wallet_1.head()
 df_wallet_2.head()
 
 # Ethereum wallet addresses
+
 wallet_addresses = list(df_wallet_0)
+
+data = []
+
+for address in wallet_addresses:
+    if address.startswith('0x'):
+        data.append(["Ethereum", address])
+    elif address.startswith('SP'):
+        data.append(["Stacks", address])
+        
 print(wallet_addresses[1])
+
 # Etherscan API endpoint and API key
 api_url = "https://api.etherscan.io/api"
 api_key = "WDP81QQFM85H8DN132FCJW8TFJF92YR9XG"
 
 for address in wallet_addresses:
-    if not address.startswith('0x'):
-        print(address)
+
 # Iterate through wallet addresses and save as list
 for address in wallet_addresses:
     # Construct the API request URL
@@ -39,15 +55,37 @@ for address in wallet_addresses:
         "address": address,
         "apikey": api_key,
     }
+    if address.startswith('0x'):
+        # Send API request
+        response = requests.get(api_url, params=params)
     
-    # Send API request
-    response = requests.get(api_url, params=params)
+        # Parse the API response
+        data = response.json()
+        balance = int(data["result"]) / 10**18  # Convert balance from wei to ether
     
-    # Parse the API response
-    data = response.json()
-    balance = int(data["result"]) / 10**18  # Convert balance from wei to ether
-    
-    # Print the balance
-    print(f"Address: {address}")
-    print(f"Balance: {balance} ETH")
-    print("---")
+        # Print the balance
+        print(f"Address: {address}")
+        print(f"Balance: {balance} ETH")
+        print("---")
+
+# Return transaction history of wallet address
+for address in wallet_addresses:
+    # Construct the API request URL
+    params = {
+        "module": "account",
+        "action": "txlist",
+        "address": address,
+        "startblock": 0,
+        "endblock": 99999999,
+        "page": 1,
+        "offset": 10,
+        "sort": 'asc',
+        "apikey": api_key,
+    }
+    if address.startswith('0x'):
+        response = requests.get(api_url, params=params)
+        response_parsed = json.loads(response.content)
+        txs = response_parsed['result']
+        print([ {'from': tx['from'], 'to': tx['to'], 'value': int(tx['value'])/10**18, 'timestamp': tx['timeStamp']} \
+        for tx in txs ])
+
